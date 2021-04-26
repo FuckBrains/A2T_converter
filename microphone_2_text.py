4f340057b6b3eab52020f412f1a564b41a45a2a2
sveneschlbeck <sven.eschlbeck@t-online.de>
2021-04-26 16:13:06 +0200

    refactoring_mic_2_text

--- microphone_2_text.py
+++ microphone_2_text.py
@@ -5,34 +5,64 @@ import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-# Welcome message and information
-print("\n###########################################################")
-print("...........................................................")
-print("...........................................................")
-print("...........................................................")
-print("############## Mic2T by Sven Eschlbeck 2021 ###############")
-print("...........................................................")
-print("...........................................................")
-print("...........................................................")
-print("###########################################################")
-print("Hello! Welcome to Microphone2Text.")
-print("Enter 'q' to quit any time.")
-print("-----------------------------------------------------")
+
+banner = r'''
+
+
+			 ######
+			########
+			########
+			########
+			  ####
+			  ####		#####   ########
+			  ####		   ##      ##
+			  ####   	#####      ##
+			   ##		##         ##
+			   ##  		#####      ##
+			   #
+			   #
+			####
+			#
+			#
+
+              		[Sven Eschlbeck] [2021]
+
+
+
+##############################################################################
+##############################################################################
+
+Hello! Welcome to 'MIC2TEXT'.
+
+Enter 'q' to quit any time.
+
+##############################################################################
+##############################################################################
+'''
+
+print(banner)
+
+
+prompt = r"""
+Please specify the language of the submitted YouTube video.
+
+Press '1' for French.			Press '2' for German.			Press '3' for Italian.
+Press '4' for Russian.			Press '5' for Dutch.			Press '6' for Mandarin (Han Yu).
+Press '7' for Spanish.  		Press '8' for Turkish.  		Press '9' for Swedish.
+Press '10' for Portuguese.		Press '11' for Japanese.  		Press '12' for Korean.
+Press '13' for Polish. 			Press '14' for Czech.  			Press '15' for Finnish.
+Press '16' for Hebrew. 			Press '17' for Hungarian.		Press '18' for Indonesian.
+Press '19' for Malaysian.		Press '20' for Norwegian.		Press '21' for Romanian.
+Press '22' for Serbian.   		Press '23' for Slovak.   		Press '24' for Afrikaans.
+
+Leave blank for default (English).
+"""
+
 
 
 while True:
-	# Asking for the language of the given speech
-	prompt = """Please specify the language of the submitted YouTube video.\n
-	Press '1' for French.			Press '2' for German.			Press '3' for Italian.
-	Press '4' for Russian.			Press '5' for Dutch.			Press '6' for Mandarin (Han Yu).
-	Press '7' for Spanish.  		Press '8' for Turkish.  		Press '9' for Swedish.
-	Press '10' for Portuguese.		Press '11' for Japanese.  		Press '12' for Korean.
-	Press '13' for Polish. 			Press '14' for Czech.  			Press '15' for Finnish.
-	Press '16' for Hebrew. 			Press '17' for Hungarian.		Press '18' for Indonesian.
-	Press '19' for Malaysian.		Press '20' for Norwegian.		Press '21' for Romanian.
-	Press '22' for Serbian.   		Press '23' for Slovak.   		Press '24' for Afrikaans.
-	Leave blank for default (English).\n"""
 
+	print("------------------------------------------------------------------------------")
 	lang = input(prompt)
 
 	if lang == '1':
@@ -89,8 +119,10 @@ while True:
 		break
 
 
+	print("------------------------------------------------------------------------------\n")
+
 	# Asking for the duration of the planned recording
-	length = input("\nHow long do you plan to speak?\nDefine a time span, press '1' for 30 seconds.\nPress '2' for 60 seconds.\nPress '3' for 90 seconds.\nPress '4' for 120 seconds.\nPress '5' for 300 seconds.\nLeave blank for default (10 seconds).\n")
+	length = input("How long do you plan to speak?\n\nDefine a time span.\nPress '1' for 30 seconds.\nPress '2' for 60 seconds.\nPress '3' for 90 seconds.\nPress '4' for 120 seconds.\nPress '5' for 300 seconds.\n\nLeave blank for default (10 seconds).\n")
 	if length == '1':
 		duration = 30
 	if length == '2':
@@ -113,8 +145,9 @@ while True:
 	# Defining microphone as input source
 	with sr.Microphone() as source:
 		# Users starts speaking
-		print("\nSpeak now!")
-		print("-----------------------------------------------------")
+		print("\n------------------------------------------------------------------------------\n")
+		print("Speak now!")
+		print("\n--------------------------------------------------------------------------------\n")
 		# Read the audio data from the default microphone
 		audio_data = r.record(source, duration = duration)
 		# Printing wait statement
@@ -122,14 +155,16 @@ while True:
 		# Convert speech to text
 		text = r.recognize_google(audio_data, language = language)
 		# Print text
-		print("-----------------------------------------------------")
+		print("\n------------------------------------------------------------------------------\n")
 		print(f"Record:\n{text}")
-		print("-----------------------------------------------------")
+		print("\n------------------------------------------------------------------------------\n")
 
-	question_save = input("Do you want to save the transcription to a text file? [y/n]")
+	question_save = input("Do you want to save the transcription to a text file? [y/n]\n")
 	if question_save == 'y':
+		print("\n------------------------------------------------------------------------------\n")
 		# Ask for the destination folder where the text should be stored
 		destination_folder = input("Please specify the full path to the desired destination folder.\nYour entry should look like this: 'C:/User/...' but without quotes.\n")
+		print("\n------------------------------------------------------------------------------\n")
 		# Ask for the file name
 		file_name = input("Please specify the desired file name.\n")
 
@@ -139,9 +174,9 @@ while True:
 			# Write the text into the file
 			f.write(f"{text}")
 
-		print("-----------------------------------------------------")
+		print("\n------------------------------------------------------------------------------\n")
 		print("Your text file is created...\nGo to the folder you specified to access it.")
-		print("-----------------------------------------------------")
+		print("\n------------------------------------------------------------------------------\n")
 
 	if question_save == 'n':
 		pass
@@ -150,7 +185,7 @@ while True:
 
 	question_more = input("Do you wish to record another speech? [y/n]\n")
 	if question_more == 'y':
-		print("-----------------------------------------------------")
+		print("\n------------------------------------------------------------------------------\n")
 		continue
 	if question_more == 'n':
 		break
